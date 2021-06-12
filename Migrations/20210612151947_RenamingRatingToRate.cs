fd842414d720b9968c5b946baff20a1908e1b6dc
MainaGeorge <nuriszka@gmail.com>
2021-06-12 17:26:48 +0200

    added database with initial seeding

---
+++ Migrations/20210612151947_RenamingRatingToRate.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace my_books.Migrations
+{
+    public partial class RenamingRatingToRate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Rating",
+                table: "Books",
+                newName: "Rate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Rate",
+                table: "Books",
+                newName: "Rating");
+        }
+    }
+}
